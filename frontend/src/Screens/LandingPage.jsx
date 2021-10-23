fb64119a91813098a265fe53aeea77a36adcd87f
Aaron J <aaron.janes@gmail.com>
2021-10-23 10:01:08 -0600

    updated the landing page

--- frontend/src/Screens/LandingPage.jsx
+++ frontend/src/Screens/LandingPage.jsx
@@ -8,52 +8,21 @@ const LandingPage = () => {
   return (
     <Container>
       <Col md={12}>
-        <Image data-aos='fade-up' src='wbc-poster2.jpg' alt='event poster' fluid />
-        <h1 className='site-title' data-aos='fade-up' style={{ fontSize: '3rem', textAlign: 'center' }}>
-          {' '}
+        <Image data-aos='fade-up' src='wbc-logo-no-txt.png' alt='company logo' fluid />
+        <h1 data-aos='fade-up' className='site-title' style={{ fontSize: '3rem', textAlign: 'center' }}>
           THE WILD BOYS CAMPOUT
         </h1>
-        <h6 style={{ textAlign: 'center', lineHeight: '2.5rem', fontSize: ' .7rem' }}>
-          <h3 data-aos='fade-up'>
-            SEASON ENDER !! WILD BOYZZZ!! <br />
-          </h3>
+        <h6 data-aos='fade-up' style={{ textAlign: 'center', lineHeight: '2.5rem', fontSize: ' .7rem' }}>
+          The Wild Boys Camp-Out was established in 2020 by a bunch of blue collar, beer drinking, eagle riding motorcycle hooligans. We started by spontaneously putting together a casual last minute camp-out that more
+          than 100 people attended. When this began we never had the intentions to make it an annual event but because of how memorable the camp-outs turned out, we decided to make this a summer event in hopes of
+          bringing even more of the motorcycle community together.
           <br />
-          <span data-aos='fade-up'>
-            LETS CLOSE THE SEASON OUT RIGHT WITH A DEADLY PARTY! . Tickets at the gate 40 dollars. . <br /> <br />
-          </span>
-          <h4 data-aos='fade-up'>
-            LOCATION <br />
-          </h4>
-          <span data-aos='fade-up'>
-            Six Pak saloon 22524 Highway 37 Sturgeon County AB T8L 5E6 Canada <br />
-          </span>
           <br />
-          <h4 data-aos='fade-up'>
-            MUSIC <br />
-          </h4>
-          <span data-aos='fade-up'>
-            &nbsp; &nbsp; @el_niven &nbsp; @upsidedowntown.ca &nbsp;&nbsp; DJ Skoden <br /> <br />
-          </span>
-          <span data-aos='fade-up'>
-            STAY THE NIGHT PITCH YOUR TENT CAMPOUT! Everyone is welcome tons of parking for trailers motorcycles or whatever u wanna bring. Pitch a tent remember this is still a campout! Plus if you don’t wanna stay the
-            night but wanna drink a few cold ones… guess what cabs and Uber will pick you up and drive you home safely. .<br />
-            <br />
-          </span>
-          <span data-aos='fade-up'>
-            This is not a party you wanna miss. Live music the best local vendors and company’s. 18+ BYOB or drinks by donations at the saloon. . Food trucks on site. . We will be collecting donations for @kwcsyeg On
-            location. . <br />
-            <br />
-          </span>
-          <span data-aos='fade-up'>
-            Plus tons more exciting stuff if you’ve been to a wild boys campout you know what to expect. . <br />
-            <br />
-          </span>
-          <h4 data-aos='fade-up'>VENDORS / SPONSORS</h4>
+          The Wild Boys Camp Out welcomes anyone and everyone we just ask that you respect one another, the environment and your surroundings.
           <br />
-          <span data-aos='fade-up'>
-            @tn_industries @tripsixkustoms @lifesentencecycles @hdedmonton @death.defied @eville.empire @odinmfg @fuel_the_chaos @northern_cycles @showdowntattoos @fkndeadly @skulldefy @kevin_lemire_designer @topyography
-            @3astc0ast1 @broadsquadinc_ @kelevramotorcycles @hamiltonaudio @world_famous_sideshow_tattoo @coyoteblonde @rocking.chair.spirits @elite_touring_innovations
-          </span>
+          <br />
+          Stay safe, stay wild. <br />
+          Wild Boys Camp Out
         </h6>
         <Row className='d-flex justify-content-center align-items-center'>
           <Button className='btn col-4 m-3 rounded' href='https://www.instagram.com/thewildboyscampout/?hl=en' variant='warning' target='_blank'>
